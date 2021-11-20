c0fe350f92e80b49e68ea9fe981b7942d9b0c9db
NMDSilva <silvanuno@gmail.com>
2021-11-20 21:48:34 -0100

    Carregar dados apenas com dados do card

--- obter_compraremcasa/getDados.py
+++ obter_compraremcasa/getDados.py
@@ -2,7 +2,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
-from ..db.orm_bd import *
+from orm_bd import *
 import datetime
 
 load_dotenv("../.env")
@@ -30,24 +30,37 @@ class GetDados():
   
   def categoria_existe(self, categoria):
     return Categoria.select().where(Categoria.nome == categoria).exists()
-
-  def get_info_produto(self, slugProduto, categoria):
-    pagina = urllib.request.urlopen(self.url + "/" + slugProduto).read()
+  
+  def carregar_produtos(self, categoria, page = 1):
+    pagina = urllib.request.urlopen(self.url + categoria + self.filtro + str(page)).read()
     html = BeautifulSoup(pagina, "html.parser")
-    if html.find("span", class_ = "form-price") != None:
-      titulo = html.find("div", class_ = "brand").text.strip()
-      descricao = html.find("div", class_ = "form-group").find("h1").text.strip()
-      precoUnitario = html.find("span", class_ = "form-price").text.replace("€", "").strip()
-      block = html.find("span", class_ = "product-block-list").text.strip().split("/")
-      try: sku = html.find("span", class_ = "sku_elem").text.strip()
-      except: sku = None
-      preco = block[0].replace("€", "").strip()
-      unidade = block[1].strip()
-      try: img = html.find("img", class_="img-fluid")["src"]
-      except: img = None
-      self.registar_produto(slugProduto, titulo, descricao, precoUnitario, preco, img, categoria, unidade, sku)
+    produtos = html.find_all("div", class_ = "product-block")
+    if len(produtos) > 0:
+      for produto in produtos:
+        if produto.find("form") != None:
+          slug, descricao, precoUnitario, preco, img, unidade = self.get_info_produto(produto)
+          if(self.novo_produto_ou_preco(slug)):
+            self.registar_produto(slug, descricao, precoUnitario, preco, img, categoria, unidade)
 
-  def registar_produto(self, slugProduto, titulo, descricao, precoUnitario, preco, img, categoria, unidade, sku):
+      self.carregar_produtos(categoria, page + 1)
+
+  def get_info_produto(self, produto):
+    slug = produto.find("a", class_ = "product-image")["href"][1:]
+    descricao = produto.find("h3").find("a").text.strip()
+    blocoPrecos = produto.find("div", class_ = "list-price")
+    precoUnitario = blocoPrecos.findChildren("span", class_ = "product-block-normal", recursive = False)
+    if len(precoUnitario) > 0:
+      precoUnitario = precoUnitario[0].text.replace("€", "").strip()
+    else:
+      precoUnitario = blocoPrecos.findChildren("span", class_ = "product-block-list", recursive = False)[0].text.replace("€", "").strip()
+    block = blocoPrecos.findChildren("div", recursive = False)[0].find("span", class_ = "product-block-list").text.strip().split("/")
+    preco = block[0].replace("€", "").strip()
+    unidade = block[1].strip()
+    try: img = produto.find("img")["src"]
+    except: img = None
+    return slug, descricao, precoUnitario, preco, img, unidade
+
+  def registar_produto(self, slugProduto, descricao, precoUnitario, preco, img, categoria, unidade):
     id_categoria = Categoria.get(Categoria.nome == categoria).get_id()
 
     unidade, create = Unidade.get_or_create(nome = unidade)
@@ -55,10 +68,8 @@ class GetDados():
     produto, create = Produto.get_or_create(
       slug = slugProduto,
       defaults = {
-        'titulo': titulo,
         'descricao': descricao,
         'img': img,
-        'sku': sku,
         'categoria_id': id_categoria
       }
     )
@@ -72,17 +83,6 @@ class GetDados():
 
   def novo_produto_ou_preco(self, slug):
     return not Produto.select().join(Preco).where(Produto.slug == slug & Preco.created_at >= (datetime.datetime.now() - datetime.timedelta(days=3))).exists()
-
-  def carregar_produtos(self, categoria, page = 1):
-    pagina = urllib.request.urlopen(self.url + categoria + self.filtro + str(page)).read()
-    html = BeautifulSoup(pagina, "html.parser")
-    produtos = html.find_all("div", class_ = "product-block")
-    if len(produtos) > 0:
-      for produto in produtos:
-        slug = produto.find("a", class_ = "product-image")["href"][1:]
-        if(self.novo_produto_ou_preco(slug)):
-          self.get_info_produto(slug, categoria)
-      self.carregar_produtos(categoria, page + 1)
 
   def iniciar(self):
     self.carregar_categorias()
